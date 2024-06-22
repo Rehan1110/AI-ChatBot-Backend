fdaf9b632fb8c40744565292d32b0a6af9037031
Rehan Abbas <abbasrehan054@gmail.com>
2024-06-22 19:35:23 +0530

    Connected to the database

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
-import express from "express";
-import { config } from "dotenv";
-config();
-const app = express();
-app.use(express.json());
-app.listen(5000, () => {
-    console.log("Server Open");
-});
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
+const PORT = process.env.PORT || 5000;
+connectToDatabase()
+    .then(() => {
+    app.listen(PORT, () => console.log("Server Open"));
+})
+    .catch((err) => console.log(err));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express from "express";
-import { config } from "dotenv";
-config();
-const app = express();
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
 
-app.use(express.json());
+const PORT = process.env.PORT || 5000
+connectToDatabase()
+.then(() => {
+  app.listen(PORT, () => 
+    console.log("Server Open")
+  );
+})
+.catch((err) => console.log(err));
 
-app.listen(5000, () => {
-  console.log("Server Open");
-});
